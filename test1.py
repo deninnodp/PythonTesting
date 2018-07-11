3fff7e81312c061989635369dda31ee7e1eda062
deninnodp <unconfigured@null.spigotmc.org>
2018-07-10 20:29:16 -0400

    I DID IT!

--- test1.py
+++ test1.py
@@ -9,3 +9,5 @@ num2 = input('NOW A SECOND ONE YOU NUMBER SLAVE: ')
 sum = int(num1) + int(num2)
 
 print("YO SON, THE SUM OF " + str(num1) + " AND " + str(num2) + " IS " + str(sum))
+
+#I DID IT I COMMITTED IT
