a43f3a1aeb9de6bb05382435e6b1b9dd7f23b087
deninnodp <unconfigured@null.spigotmc.org>
2018-07-10 20:26:38 -0400

    I DID IT?

---
+++ test1.py
@@ -0,0 +1,11 @@
+## First Test
+##Also I am drinking so suck it
+
+num1 = input('ENTER A NUMBER YOU BEAN BOI: ')
+num2 = input('NOW A SECOND ONE YOU NUMBER SLAVE: ')
+
+# Add that shit son
+
+sum = int(num1) + int(num2)
+
+print("YO SON, THE SUM OF " + str(num1) + " AND " + str(num2) + " IS " + str(sum))
